97ada3fab24f3682080ddd6102696bc03c1d269a
aymendakir <aymendakr@yahoo.com>
2024-10-06 23:57:31 +0100

    m

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -17,11 +17,11 @@ class VerifyCsrfToken extends Middleware
         'stripe/*',
         'http://example.com/foo/bar',
         'http://localhost:8000/*',
-        "",
+        
         "http://127.0.0.1:8000/*",
         "https://hotel-back-gdee.vercel.app/*",
         "https://hotel-front-indol.vercel.app/*",
-        "https://hotel-back-gdee.vercel.app",
-        "https://hotel-front-indol.vercel.app"
+        "https://hotel-back-gdee.vercel.app/*",
+        "https://hotel-front-indol.vercel.app/*"
     ];
 }
